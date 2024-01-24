8fbbbfae8f32623087115a5003cfbe7289983d88
vahid <vahid.sh1431@yahoo.com>
2024-01-24 12:35:59 +0330

    add update and delete view

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.BookListView.as_view(), name='book_list'),
     path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
     path('create/', views.BookCreateView.as_view(), name='book_create'),
+    path('<int:pk>/update/', views.BookUpdateView.as_view(), name='book_update'),
+    path('<int:pk>/delete/', views.BookDeleteView.as_view(), name='book_delete'),
+
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Book
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 
 class BookListView(generic.ListView):
@@ -17,3 +19,22 @@ class BookCreateView(generic.CreateView):
     template_name = 'books/book_create.html'
     model = Book
     fields = '__all__'
+
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = '__all__'
+    template_name = 'books/book_update.html'
+
+    def form_valid(self, form):
+        if form.has_changed():
+            form.save()
+            return redirect('book_detail', pk=self.object.pk)
+        else:
+            return redirect('book_update', pk=self.object.pk)
+
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book_list')
