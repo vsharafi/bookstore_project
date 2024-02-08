d1940369bf92bebfd6d7dc4249c5a72979336c06
vahid <vahid.sh1431@yahoo.com>
2024-02-08 11:14:43 +0330

    added related_name for comment model

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'user', 'book', 'datetime_created')
+
 
 admin.site.register(Book)
+# admin.site.register(Comment, CommentAdmin)
